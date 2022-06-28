a527803eb0ee48356cf11dd9d2dbced0f1ee9573
Janne Mattila <janne.mattila@hotmail.fi>
2022-06-28 10:07:00 +0300

    Update scripts

--- Export-OpenAPIs.ps1
+++ Export-OpenAPIs.ps1
@@ -6,24 +6,54 @@ Param (
     [string] $APIMName,
 
     [Parameter(HelpMessage = "Export folder")] 
-    [string] $ExportFolder = "$PSScriptRoot\Export"
+    [string] $ExportFolder = "Export",
+
+    [Parameter(HelpMessage = "Export file")] 
+    [string] $OutputFilename = "Output.yaml",
+
+    [Parameter(HelpMessage = "Merge configuration file")] 
+    [string] $ConfigurationFilename = "configuration.json"
 )
 
 $ErrorActionPreference = "Stop"
 
 "Exporting Azure API Management APIs to: $ExportFolder"
+"Output OpenAPI file: $OutputFilename"
+
 New-Item -Path $ExportFolder -ItemType Directory -Force | Out-Null
 
 $apiManagementContext = New-AzApiManagementContext -ResourceGroupName $ResourceGroupName -ServiceName $APIMName
 $allAPIs = Get-AzApiManagementApi -Context $apiManagementContext
 
-$api = $allAPIs[0]
+$configuration = @{
+    inputs = @()
+    output = $OutputFilename
+}
+
+$exportedSuccessfully = 0
+$exportFailures = 0
 foreach ($api in $allAPIs) {
     $api.ApiId
     $apiFile = Join-Path -Path $ExportFolder -ChildPath "$($api.ApiId).yaml"
-    Export-AzApiManagementApi -Context $apiManagementContext -ApiId $api.ApiId -SpecificationFormat OpenApi -SaveAs $apiFile -Force -ErrorAction Continue
+
+    try {
+        Export-AzApiManagementApi -Context $apiManagementContext -ApiId $api.ApiId -SpecificationFormat OpenApi -SaveAs $apiFile -Force
+        $exportedSuccessfully += 1
+        $configuration.inputs += @{ 
+            inputFile        = $apiFile
+            pathModification = @{
+                prepend = $api.Path
+            }
+        }
+    }
+    catch {
+        Write-Warning -Message "Could not export API '$($api.ApiId)' from API Management"
+        $exportFailures += 1
+    }
 }
 
-"Exported $($allAPIs.Count) APIs"
+"Exported successfully $exportedSuccessfully APIs and failed to export $exportFailures APIs"
+
+$configuration | ConvertTo-Json -Depth 3 > $ConfigurationFilename
 
 "Export completed"
--- Merge-OpenAPIs.ps1
+++ Merge-OpenAPIs.ps1
@@ -1,37 +1,12 @@
 Param (
-    [Parameter(HelpMessage = "OpenAPI spec folder")] 
-    [string] $Folder = "$PSScriptRoot\Export",
-
-    [Parameter(HelpMessage = "Export file")] 
-    [string] $OutputFilename = "$PSScriptRoot\Output.yaml"
+    [Parameter(HelpMessage = "Merge configuration file")] 
+    [string] $ConfigurationFilename = "$PSScriptRoot\configuration.json"
 )
 
 $ErrorActionPreference = "Stop"
 
-"Merging OpenAPIs from folder: $Folder"
-"Output OpenAPI file: $outputFile"
+"Merge configuration file: $ConfigurationFilename"
 
-$files = Get-ChildItem -Path $Folder -Filter *.yaml
-
-$configuration = @{
-    inputs = @()
-    output = $OutputFilename
-}
-
-foreach ($file in $files) {
-    $file
-    $configuration.inputs += @{ 
-        inputFile        = $file.FullName
-        pathModification = @{
-            prepend = $file.BaseName
-        }
-    }
-}
-
-"Merging $($files.Count) APIs"
-
-$configuration | ConvertTo-Json -Depth 3 > configuration.json
-
-npx openapi-merge-cli --config configuration.json
+npx openapi-merge-cli --config $ConfigurationFilename
 
 "Merge completed"
